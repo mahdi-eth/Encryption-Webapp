ee0eef4b9ad3dbd94e0631d756688cfa9ec76044
Mahdi-Ettehadnejad <mahdi.ettehad85@gmail.com>
2023-02-01 15:15:25 +0330

    Staging for beggining to code

---
+++ view/src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./index.css";
+
+function App() {
+    return <div className="App">ok</div>;
+}
+
+export default App;
